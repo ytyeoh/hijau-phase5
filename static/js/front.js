3f7ce61b1680f2be389865252b3a559d681b0504
ytyeoh <ytyeoh@me.com>
2021-07-15 14:46:23 +0800

    descripton section done

--- static/js/front.js
+++ static/js/front.js
@@ -120,6 +120,24 @@ function sliders () {
       lazyLoad: true
     })
 
+    $('.desciption-page').owlCarousel({
+      dotsContainer: '#carousel-custom-dots',
+      navigation: true, // Show next and prev buttons
+      navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+      slideSpeed: 2000,
+      paginationSpeed: 1000,
+      autoPlay: true,
+      stopOnHover: true,
+      singleItem: true,
+      lazyLoad: false,
+      addClassActive: true,
+      afterInit: function () {
+        // animationsSlider()
+      },
+      afterMove: function () {
+        // animationsSlider()
+      }
+    })
     $('.homepage').owlCarousel({
       dotsContainer: '#carousel-custom-dots',
       navigation: true, // Show next and prev buttons
